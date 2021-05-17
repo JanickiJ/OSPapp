9b642301a951697925892730c4daf2cd50c960dd
Anna Gut <annagut026@gmail.com>
2021-05-17 12:34:34 +0200

    Delete .buildozer directory

--- .buildozer/android/platform/build-armeabi-v7a/dists/ospapp__armeabi-v7a/obj/local/armeabi-v7a/objs-debug/cpufeatures/cpu-features.o.d
+++
@@ -1,5 +0,0 @@
-/home/agut/Desktop/OSPAPP/OSPapp/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/obj/local/armeabi-v7a/objs-debug/cpufeatures/cpu-features.o: \
-  /home/agut/.buildozer/android/platform/android-ndk-r19c/sources/android/cpufeatures/cpu-features.c \
-  /home/agut/.buildozer/android/platform/android-ndk-r19c/sources/android/cpufeatures/cpu-features.h
-
-/home/agut/.buildozer/android/platform/android-ndk-r19c/sources/android/cpufeatures/cpu-features.h:
